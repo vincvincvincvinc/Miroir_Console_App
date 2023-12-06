9fa52026fb33b65bbcf53f0c72d3a14e73a45b65
vincvincvincvinc <152969599+vincvincvincvinc@users.noreply.github.com>
2023-12-06 09:21:54 +0100

    First Commit

---
+++ src/main/java/fr/epsi/b32324c1/MiroirConsoleApp.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class MiroirConsoleApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        saluer();
+
+        // Main loop
+        while (true) {
+            System.out.print("Entrez du texte (ou 'exit' pour quitter) : ");
+            String input = scanner.nextLine();
+
+            // Palindrome
+            if (estPalindrome(input)) {
+                System.out.println("Bien dit!");
+            } else {
+                // Miroir
+                System.out.println("En miroir : " + inverserChaine(input));
+            }
+
+            // Quitter
+            if (input.equalsIgnoreCase("exit")) {
+                // Au revoir
+                direAuRevoir();
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void saluer() {
+        Calendar calendar = Calendar.getInstance();
+        int heure = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (heure >= 6 && heure < 12) {
+            System.out.println("Labas rytas!");
+        } else if (heure >= 12 && heure < 18) {
+            System.out.println("Laba diena!");
+        } else {
+            System.out.println("Labas vakaras!");
+        }
+    }
+
+    private static boolean estPalindrome(String texte) {
+        String texteSansEspaces = texte.replaceAll("\\s", "").toLowerCase();
+        String inverse = new StringBuilder(texteSansEspaces).reverse().toString();
+        return texteSansEspaces.equals(inverse);
+    }
+
+    private static String inverserChaine(String texte) {
+        return new StringBuilder(texte).reverse().toString();
+    }
+
+    private static void direAuRevoir() {
+        Calendar calendar = Calendar.getInstance();
+        int heure = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (heure >= 6 && heure < 12) {
+            System.out.println("Iki!");
+        } else if (heure >= 12 && heure < 18) {
+            System.out.println("Geros dienos!");
+        } else {
+            System.out.println("Gero vakaro!");
+        }
+    }
+}
